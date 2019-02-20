eecb696e96b638575399b04832585abd98cebada
Valtrius <Valtrius@hotmail.com>
2019-02-20 21:51:43 +0000

    converted the Swatch function to a class for setTimeout reasons

--- src/modes/Swatch.js
+++ src/modes/Swatch.js
@@ -1,71 +1,93 @@
 import React, {useState, useEffect} from 'react'
 import { CSSTransitionGroup } from 'react-transition-group'
 
-const Swatch = () => {
+class Swatch extends React.Component {
 
-    const [count, setCount] = useState(0)
-    const [swatches, setSwatches] = useState([])
-    const [game, setGame] = useState("intro")
-    const [timer, setTimer] = useState()
-
-    //Need to check the 10th click to see if success/fail
-
-    const onClick = e => {
-        e.preventDefault()
-        if(count >= swatches.length-1){
-            setCount(0)
-            setSwatches([])
-            setGame("success")
-        }else if(
-            (swatches[count] === 0 && e.type === "contextmenu")
-            || (swatches[count] === 1 && e.type === "click")
-        ){
-            setCount(count+1)
-        }
-        else{
-            setCount(0)
-            setGame("fail")
+    constructor(){
+        super()
+        this.num = 20
+        this.timeLimit = 10000
+        this.state = {
+            count: 0,
+            swatches: [],
+            game: "intro"
         }
     }
 
-    const classes = () => {
+    onClick = e => {
+        e.preventDefault()
+        const {count, swatches, game} = this.state
+        const correct = (swatches[count] === 0 && e.type === "contextmenu")
+        || (swatches[count] === 1 && e.type === "click")
+        if(correct){
+            this.setState({
+                count: count+1
+            })
+
+            if(count === swatches.length-1){
+                this.setState({
+                    count: 0,
+                    swatches: [],
+                    game: "success"
+                })
+                clearInterval(this.timer)
+            }
+
+        }else{
+            this.setState({
+                count: 0,
+                game: "fail"
+            })
+            clearInterval(this.timer)
+        }
+    }
+
+    classes = () => {
+        const {game, swatches, count} = this.state
         console.log("classes")
         let classes = ["square"]
         if(game === "start") classes.push(swatches[count] === 0 ? "red" : "blue")
         if(game === "intro") classes.push("border-b")
         else if(game === "success") classes.push("border-g")
         else if(game === "fail") classes.push("border-r")
+        else if(game === "timer") classes.push("border-r")
         return classes.join(" ")
     }
 
-    console.log(timer)
-
-    const start = () => {
+    start = () => {
+        const {swatches} = this.state
         if(!swatches.length){
-            Array(10).fill().forEach((v, i) => {
+            Array(this.num).fill().forEach((v, i) => {
                 swatches.push(Math.floor(Math.random() * 2))
             })
         }
         
-        if(!timer){
-            console.log("begin")
-            setTimer(setTimeout(()=>{
-                //if(game === "start"){
-                    console.log(game)
-                    //setGame("fail")
-            }, 10000))
-        }
-        setGame("start");
+        this.now = Date.now()
+        this.timer = setTimeout(()=>{
+            this.setState({
+                count: 0,
+                game: "timer"                
+            })
+        }, this.timeLimit)
+
+        this.setState({
+            game: "start"
+        })
     }
 
-    if(game === "start"){
-        return <div className={classes()} onClick={onClick} onContextMenu={onClick}></div>
-    }else if(game === "intro"){
-        return <div className={classes()} onClick={start}><h3>Swatch</h3>Different coloured swatches will appear in the square, you must press the correct mouse button when the coloured swatch appears. Right is for Red and Left is for Blue. You must complete it within 10 seconds.<br/><br/>Click the Square to begin.</div>
-    }else if(game === "success"){
-        return <div className={classes()} onClick={()=>setGame("intro")}>Well Done!</div>
-    }else if(game === "fail"){
-        return <div className={classes()} onClick={()=>setGame("intro")}>Too bad :( Click to try again</div>
+    render(){
+        const {game} = this.state
+        if(game === "start"){
+            return <div className={this.classes()} onClick={this.onClick} onContextMenu={this.onClick}></div>
+        }else if(game === "intro"){
+            return <div className={this.classes()} onClick={this.start}><h3>Swatch</h3>Different coloured swatches will appear in the square, you must press the correct mouse button when the coloured swatch appears. Right is for Red and Left is for Blue. You must complete it within {this.timeLimit/1000} seconds.<br/><br/>Click the Square to begin.</div>
+        }else if(game === "success"){
+            return <div className={this.classes()} onClick={()=>this.setState({game:"intro"})}><h3>Well Done!</h3>You had {(this.timeLimit - (Date.now() - this.now))/1000}s remaining</div>
+        }else if(game === "fail"){
+            return <div className={this.classes()} onClick={()=>this.setState({game:"intro"})}><h3>Wrong Click</h3>You clicked the wrong button on the mouse :(</div>
+        }else if(game === "timer"){
+            return <div className={this.classes()} onClick={()=>this.setState({game:"intro"})}><h3>Too Slow</h3>The timer has expired because you were too slow. Try going faster next time.</div>
+        }
     }
 }
 
