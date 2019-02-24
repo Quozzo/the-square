9ad2b56b2ae1e50ca256a0cd8de99d3b37d44c2d
Valtrius <Valtrius@hotmail.com>
2019-02-24 01:29:51 +0000

    Added in react router

---
+++ src/Manager.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import {BrowserRouter as Router, Link} from 'react-router-dom'
+
+export default class Manager extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
