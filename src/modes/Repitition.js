ba1e7ae54b21c5db8d3e8b9b9d34ccf7d9df6471
Valtrius <Valtrius@hotmail.com>
2019-03-02 01:45:56 +0000

    Working example without any user feedback or if wrong. Infinite length

--- src/modes/Repitition.js
+++ src/modes/Repitition.js
@@ -7,7 +7,6 @@ export default class Repitition extends Component {
             phase: "intro",
             step: 0,
             steps: 0,
-            ai: true,
             sequence: [[this.rand(5), this.rand(5)]]
         }
     }
@@ -18,19 +17,35 @@ export default class Repitition extends Component {
 
     onClick = () => {
         this.setState({
-            phase: "start"
+            phase: "ai"
         })
     }
 
-    guess = (x, y) => {
-        const {sequence} = this.state
+    nextStep = () =>{
         let {step, steps} = this.state
+        if(step < steps){
+            this.setState({
+                step: ++step
+            })
+        }else{
+            this.setState({
+                phase: "start",
+                step: 0
+            })
+        }
+    }
+
+    guess = (x, y) => {
+        const {sequence, phase} = this.state
+        let {step, steps} = this.state
+        if(phase === "ai") return
         if(x === sequence[step][0] && y === sequence[step][1]){
             if(step === steps){// if at end of sequence
                 this.setState({
                     sequence: [...sequence, [this.rand(5), this.rand(5)]],
-                    step: ++step,
-                    steps: ++steps
+                    steps: ++steps,
+                    phase: "ai",
+                    step: 0
                 })
             }else if(step < steps){ //correct but not at end
                 this.setState({
@@ -41,10 +56,10 @@ export default class Repitition extends Component {
     }
 
     render() {
-        const {phase, sequence, step} = this.state
+        const {phase, sequence, step, hidden} = this.state
         if(phase === "intro"){
             return <div className="square border-b" onClick={this.onClick}><h3>Repitition</h3>Remember the sequence which will gradular harder everytime you get it right.</div>
-        }else if(phase === "start"){
+        }else if(phase === "start" || phase === "ai"){
             return (
                 <div className="square">
                     <table><tbody>
@@ -52,7 +67,8 @@ export default class Repitition extends Component {
                             <tr key={`row-${cellY}`}>
                                 {Array(5).fill().map((v, cellX) => (
                                     <td key={`cell=${cellX}-${cellY}`} onClick={()=>this.guess(cellX, cellY)}>
-                                        {cellX === sequence[step][0] && cellY === sequence[step][1] && <Square />}
+                                        {!hidden && phase === "ai" && cellX === sequence[step][0] && cellY === sequence[step][1] && <Square phase={phase} nextStep={this.nextStep} />}
+                                        {/*phase === "start" && sequence[step-1] && sequence[step][0] === cellX && sequence[step][0] === cellY && <Square phase={phase} nextStep={this.nextStep} />*/ }
                                     </td>
                                 )) }
                             </tr>
@@ -65,11 +81,38 @@ export default class Repitition extends Component {
 }
 
 class Square extends Component {
+    constructor(){
+        super()
+        this.state = {
+            display: true
+        }
+    }
+
+    componentDidMount(){
+    const {phase, nextStep, hidden} = this.props 
+        this.timer = setTimeout(()=>{
+            if(phase === "ai"){
+                nextStep()
+            }else{
+                this.setState({
+                    display: false
+                })
+            }
+        }, 1000)
+    }
+
+    componentWillUnmount(){
+        clearTimeout(this.timer)
+        clearTimeout(this.nextSquare)
+    }
+
+    classes = () => {
+        return !this.state.display ? "hidden" : this.props.phase === "ai" ? "blue" : "green"
+    }
+
     render() {
         return (
-            <div className="red">
-
-            </div>
+            <div className={this.classes()}></div>
         )
     }
 }
