bf27250a862b9f0e508fc2b3f3c59b1d5989c13c
Valtrius <Valtrius@hotmail.com>
2019-03-03 02:09:00 +0000

    Created a working pathing generator

--- src/modes/Pathfinder.js
+++ src/modes/Pathfinder.js
@@ -4,17 +4,75 @@ export default class Pathfinder extends Component {
     constructor(){
         super()
         this.state = {
-            phase: "intro"
+            phase: "intro",
+            size: 5,
+            coords: [],
         }
     }
 
-    generatePath = () => {
-        
+    rand = (max) => {
+        return Math.floor(Math.random() * max)
     }
-    
+
+    corners = () => {
+        const { size } = this.state
+        const coords = []
+        const path = [[0,0]]
+
+        let directions = []
+        let direction = true
+        let hundred = 100
+
+        coords[0] = []
+        coords[0][0] = 1
+
+        while(hundred--){
+            directions = []
+            let coordX = path[path.length-1][0]
+            let coordY = path[path.length-1][1]
+
+            const up = coordY-1 >= 0 && !(coords[coordX] && coords[coordX][coordY-1])
+            if(up) directions.push("up")
+            const down = coordY+1 <= size-1 && !(coords[coordX] && coords[coordX][coordY+1])
+            if(down) directions.push("down")
+            const left = coordX-1 >= 0 && !(coords[coordX-1] && coords[coordX-1][coordY])
+            if(left) directions.push("left")
+            const right = coordX+1 <= size-1 && !(coords[coordX+1] && coords[coordX+1][coordY])
+            if(right) directions.push("right")
+
+            if(!directions.length) break
+
+            let dir = directions[this.rand(directions.length)]
+
+            if(dir === "up"){
+                path.push([coordX, coordY-1])
+                if(!coords[coordX]) coords[coordX] = []
+                coords[coordX][coordY-1] = path.length
+            }else if(dir === "down"){
+                path.push([coordX, coordY+1])
+                if(!coords[coordX]) coords[coordX] = []
+                coords[coordX][coordY+1] = path.length
+            }else if(dir === "left"){
+                path.push([coordX-1, coordY])
+                if(!coords[coordX-1]) coords[coordX-1] = []
+                coords[coordX-1][coordY] = path.length
+            }else if(dir === "right"){
+                path.push([coordX+1, coordY])
+                if(!coords[coordX+1]) coords[coordX+1] = []
+                coords[coordX+1][coordY] = path.length
+            }
+        }
+
+        this.setState({
+            coords,
+            path
+        })
+
+        console.log(path, coords)
+    }
 
     start = () => {
-        this.generatePath()
+        this.corners()
         this.setState({
             phase: "start"
         })
@@ -25,18 +83,18 @@ export default class Pathfinder extends Component {
     }
     
     render() {
-        const { phase } = this.state
+        const { phase, coords, path, size } = this.state
         if(phase === "intro") {
-            return <div class='square border-b' onClick={this.start}><h3>Pathfinder</h3>A path through the square will be shown briefly before it disappears. You must navigate through the square using the same path. Start at the edge and work your way through to the other side by clicking on the squares and creating your path.</div>
+            return <div className='square border-b' onClick={this.start}><h3>Pathfinder</h3>A path through the square will be shown briefly before it disappears. You must navigate through the square using the same path. Start at the edge and work your way through to the other side by clicking on the squares and creating your path.</div>
         }else if(phase === "start"){
             return (
                 <div className="square">
                     <table><tbody>
-                        {Array(10).fill().map((v, cellY) => (
+                        {Array(size).fill().map((v, cellY) => (
                             <tr key={`row-${cellY}`}>
-                                {Array(10).fill().map((v, cellX) => (
+                                {Array(size).fill().map((v, cellX) => (
                                     <td key={`cell=${cellX}-${cellY}`} onClick={()=>this.guess(cellX, cellY)}>
-                                    
+                                        {coords[cellX] && coords[cellX][cellY] && <Square x={cellX} y={cellY} number={coords[cellX][cellY]} />}
                                     </td>
                                 )) }
                             </tr>
@@ -45,5 +103,16 @@ export default class Pathfinder extends Component {
                 </div>
             )
         }
+    }
+}
+
+class Square extends Component {
+    render() {
+        const { x, y, number } = this.props
+        return (
+            <div className='blue' style={{color: "white", fontSize: "48px", display: "flex", justifyContent: "center", alignContent: "center"}}>
+                {number}
+            </div>
+        )
     }
 }
