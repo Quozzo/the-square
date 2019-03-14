6714708f89d9712308818e981eaebceafc2a3508
Valtrius <Valtrius@hotmail.com>
2019-03-14 21:10:08 +0000

    Semi working space selector

--- src/modes/Noise.js
+++ src/modes/Noise.js
@@ -3,7 +3,8 @@ import React, { Component } from 'react'
 export default class Noise extends Component {
     constructor(){
         super()
-        this.div = React.createRef();
+        this.div = React.createRef()
+        this.container = React.createRef()
         this.state = {
             phase: "intro",
             totalSquares: 5,
@@ -39,14 +40,61 @@ export default class Noise extends Component {
     mouseMove = e => {
         const { phase, coords } = this.state
         if(phase !== "start") return
-        console.log(e.pageX - e.target.offsetLeft, e.pageY - e.target.offsetTop)
-        this.div.current.style.left = (e.pageX - e.target.offsetLeft) + "px"
-        this.div.current.style.top = (e.pageY - e.target.offsetTop) + "px"
-        console.log(this.div.current)
+        let left = e.pageX
+        let top = e.pageY
+        left = left < this.container.current.offsetLeft+10 ? this.container.current.offsetLeft+10 : left
+        left = left > this.container.current.offsetLeft+500-10 ? this.container.current.offsetLeft+500-10 : left
+        top = top < this.container.current.offsetTop+10 ? this.container.current.offsetTop+10 : top
+        top = top > this.container.current.offsetTop+500-10 ? this.container.current.offsetTop+500-10 : top
+        this.left = left
+        this.top = top
+        this.div.current.style.left = (left-10)+"px"
+        this.div.current.style.top = (top-10)+"px"
+        console.log(left, top)
+    }
+
+    position = () => {
+        const { coords } = this.state
+        return {
+            position: "relative",
+            top: coords[0],
+            left: coords[1],
+            width: "30px",
+            height: "30px"
+        }
     }
 
     guess = e => {
+        const { coords } = this.state
 
+
+        let left = e.pageX
+        let top = e.pageY
+        left = left < this.container.current.offsetLeft+10 ? this.container.current.offsetLeft+10 : left
+        left = left > this.container.current.offsetLeft+500-10 ? this.container.current.offsetLeft+500-10 : left
+        top = top < this.container.current.offsetTop+10 ? this.container.current.offsetTop+10 : top
+        top = top > this.container.current.offsetTop+500-10 ? this.container.current.offsetTop+500-10 : top
+        
+        left = left - this.container.current.offsetLeft
+        top = top - this.container.current.offsetTop
+        console.log(left, coords[0], top, coords[1])
+        if(
+            left > coords[0] && left < coords[0] + 50 &&
+            top > coords[1] && top < coords[1] + 50
+        ){
+            this.setState({
+                phase: "success"
+            })
+        }else{
+            this.setState({
+                phase: "fail"
+            })
+        }
+    }
+
+    classes = () => {
+        const { phase } = this.state
+        return phase === "fail" ? "red" : phase === "success" ? "green" : "blue"
     }
 
 	render() {
@@ -64,15 +112,27 @@ export default class Noise extends Component {
                     <Square coords={coords} nextSquare={this.nextSquare}/>
                 </div>
             )
-        }else if(phase === "start"){
+        }else if(phase === "start" || phase === "fail" || phase === "success"){
             return (
-                <div className="square" style={{padding: "0", border: "1px solid blue"}} onMouseMove={this.mouseMove} onClick={this.guess}>
-                    <div className="blue" style={{position: "relative", width: "20px", height: "20px"}} ref={this.div}/>
+                <div className="square" style={{padding: "0", border: "1px solid blue"}} onMouseMove={this.mouseMove} onClick={this.guess} ref={this.container}>
+                    {phase === "success" || phase === "fail" && <div className="blue" style={this.position()} />}
+                    <div className={this.classes()} style={{position: "absolute", width: "20px", height: "20px"}} ref={this.div}/>
                 </div>
             )
         }
 	}
 }
+
+class Movable extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
 
 class Square extends Component {
     constructor(){
@@ -99,14 +159,14 @@ class Square extends Component {
             position: "relative",
             top: coords[0],
             left: coords[1],
-            width: "20px",
-            height: "20px"
+            width: "30px",
+            height: "30px"
         }
     }
 
     render() {
         return (
-            <div className="red" style={this.position()}>
+            <div className="blue" style={this.position()}>
                 
             </div>
         )
